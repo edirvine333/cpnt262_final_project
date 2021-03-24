3766ab50269c3be74ebfdb29da3b92285b454764
Ed Irvine <edwin.irvine@edu.sait.ca>
2021-03-24 15:20:39 -0600

    worked on single item route and added blank custom 404.html

--- routes/api/v0.js
+++ routes/api/v0.js
@@ -2,17 +2,50 @@
 
 //  Load modules
 const express = require('express')
+const mongoose = require('../../../_connection.js')
+
+//  Load databases
+//  Names of databases can be modified
+const Dog = require('../../../models/dog.js')
+const Subscriber = require('../../../models/subscribers.js')
+const Member = require('../../../models/members.js')
 
 //  Declares "router" and assigns it the express.Router function, which gives us the .get method
 const router = express.Router()
 
 //  Endpoints
 //  /api/v0/gallery
-//
-//  /api/v0/subscribers
-//
-//  /api/v0/members
+router.get('/api/v0/gallery', function (req, res) {
+  Dog.find(function (err, dogs){
+    if (err) {
+      res.status(404)
+      res.sendFile(__dirname +'/public/404.html')
+    }
+    res.json(dogs)
+  })
+})
 
+//  /api/v0/subscribers
+router.get('/api/v0/subscribers', function (req, res) {
+  Subscriber.find(function (err, subscribers){
+    if (err) {
+      res.status(404)
+      res.sendFile(__dirname +'/public/404.html')
+    }
+    res.json(subscribers)
+  })
+})
+
+//  /api/v0/members
+router.get('/api/v0/members', function (req, res) {
+  Member.find(function (err, members){
+    if (err) {
+      res.status(404)
+      res.sendFile(__dirname +'/public/404.html')
+    }
+    res.json(members)
+  })
+})
 
 //  Define the script as a module
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,18 @@ router.get('/gallery', function (req, res) {
   res.render('pages/gallery.ejs')
 })
 
+router.get('/gallery/:id', function (req, res) {
+  Dog.findOne({id: req.params.id}, function (err, item) {
+    if (err || !item) {
+      res.status(404)
+      res.sendFile(__dirname + '/public/404.html')
+    }
+
+    res.render('pages/single-item.ejs')
+     
+  })
+})
+
 router.get('/team', function (req, res) {
   res.render('pages/team.ejs')
 })
