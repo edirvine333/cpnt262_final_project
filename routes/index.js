82e75ee9ee1a9fe79c1ff79cea5ffde7310dec0c
nozky <norvillie.villaruel@edu.sait.ca>
2021-03-27 21:46:12 -0600

    transition to ejs partial need to continue when i got home

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,14 @@ const express = require('express')
 //  Declares "router" and assigns it the express.Router function, which gives us the .get method
 const router = express.Router()
 
-// use for views later 
+// index
+router.get('/',(req,res) => {
+  res.render('pages/index')
+})
 
+// gallery
+router.get('/gallery-list',(req,res) => {
+  res.render('pages/gallery-list')
+})
 //  Defines the script as a module
 module.exports = router
