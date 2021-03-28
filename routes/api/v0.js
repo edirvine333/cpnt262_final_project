be6d226f3b9fe094562ccc64a4e5f6b18220ce12
Karen Sule <kmsule@gmail.com>
2021-03-27 23:57:54 -0600

    fixed problem with single-image

--- routes/api/v0.js
+++ routes/api/v0.js
@@ -34,7 +34,7 @@ router.get('/gallery-item/:id', (req, res) => {
       res.render('pages/404')
     }
 
-    res.render('pages/single-item', {dog: item})
+    res.render('pages/single-item', {dog: item, pageTitle:'Single-Item'})
   })
 
 })
